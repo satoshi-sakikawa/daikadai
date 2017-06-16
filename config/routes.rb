109c8179ea337aaeaae668a6f0026f973abb0635
satoshi sakikawa <stayspike@gmail.com>
2017-06-16 11:04:44 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     end
   end
   
+  root 'top#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
